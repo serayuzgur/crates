b51e07d2ad2295c8794cd65d8863e40f7af9aad3
Stefan Zimmermann <zimmermann.code@gmail.com>
2018-07-18 08:38:31 +0200

    decoration(): Improve RegExp()s some more :)
    Restore support for inline parent sections like [target...] in new
    OutOfLine patterns

--- src/toml/decorations.ts
+++ src/toml/decorations.ts
@@ -26,11 +26,14 @@ function decoration(
   upToDateDecorator: string,
 ): Array<DecorationOptions> {
   const isOutOfLine =
-    new RegExp(`^\\s*\\[dependencies.${crate}\\]`, "gm").exec(
+    new RegExp(`^\\s*\\[(.*\\.)?dependencies.${crate}\\]`, "gm").exec(
       editor.document.getText(),
     ) !== null;
   const regex = isOutOfLine
-    ? new RegExp(`^\\s*\\[dependencies.${crate}\\]\\s*\nversion\\s*=.*`, "gm")
+    ? new RegExp(
+      `^\\s*\\[(.*\\.)?dependencies.${crate}\\]\\s*\nversion\\s*=.*`,
+      "gm",
+    )
     : new RegExp(`^\\s*${crate}\\s*=.*`, "gm");
   const decorations = [];
   while (true) {
