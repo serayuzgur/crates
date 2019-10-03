7520293d90570bd4e31610b0ea93ee606036b366
Rens Althuis <rens.althuis@gmail.com>
2019-10-03 14:34:22 +0200

    Fix #39 using SemVer package

--- src/toml/decorations.ts
+++ src/toml/decorations.ts
@@ -14,6 +14,7 @@ import {
 import { Item } from "./parser";
 import { Dependency } from "./listener";
 import { status, ReplaceItem } from "./commands";
+import { satisfies } from "semver";
 
 export const latestVersion = (text: string) =>
   window.createTextEditorDecorationType({
@@ -41,10 +42,7 @@ function decoration(
   const start = item.start;
   const end = item.end;
   const currentVersion = item.value;
-
-  const hasLatest =
-    versions[0] === currentVersion ||
-    versions[0].indexOf(`${currentVersion}.`) === 0;
+  const hasLatest = satisfies(versions[0], currentVersion || "0.0.0");
 
   const hoverMessage = new MarkdownString(`**Available Versions**`);
   hoverMessage.isTrusted = true;
