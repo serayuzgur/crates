256a28b4360fbdc0eb0684206a13eb06b5b6467e
Rens Althuis <rens.althuis@gmail.com>
2019-10-03 14:32:23 +0200

    Revert "Fix #39"
    This reverts commit 6e27acc830c7274d5e409e09e46cb6c6e1cd7ccd.

--- src/toml/decorations.ts
+++ src/toml/decorations.ts
@@ -14,9 +14,6 @@ import {
 import { Item } from "./parser";
 import { Dependency } from "./listener";
 import { status, ReplaceItem } from "./commands";
-import { parse } from "path";
-import { version } from "bluebird";
-import * as compareVersions from "compare-versions";
 
 export const latestVersion = (text: string) =>
   window.createTextEditorDecorationType({
@@ -24,47 +21,6 @@ export const latestVersion = (text: string) =>
       margin: "2em",
     },
   });
-
-function parseSemVer(str : string): RegExpMatchArray {
-  var regex = /^(=|\^|>|<|>=|<=)?(0|[1-9]\d*)\.?(?:(0|[1-9]\d*)\.?)?(0|[1-9]\d*)?(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)?(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/;
-  var matches = str.match(regex);
-  return matches ? matches.slice(1) : [];
-};
-
-function checkHasLatest(
-  versions: string[],
-  currentVersion: string
-): boolean {
-
-  var semver = parseSemVer(currentVersion);
-  if (semver.length === 0){ return false; }
-  var v = semver.slice(1,4).map((v) => (v) ? v : "0" ); // Get rid of undefined for partial versions like: x or x.0
-
-  switch (semver[0]) {
-    case null:{
-      return (
-        versions[0] === currentVersion ||
-        versions[0].indexOf(`${currentVersion}.`) === 0
-      );
-    }
-    case "^":
-      var check = true;
-      v = v.map((s) => {
-        if (!check || s === "0"){return "0";}
-        var n = +s;
-        check = false;
-        return "" + (n + 1);
-      });
-      var vmax = v.join(".");
-      if (vmax === "0.0.0"){vmax = "1.0.0";} // Exception for versions with all zeroes
-      return compareVersions(vmax, versions[0]) >= 0;
-    //TODO: implement for tilde
-    //TODO: implement for wildcard
-    //TODO: implement for comparison requirements
-    default:
-  }
-  return false;
-}
 
 /**
  * Create a decoration for the given crate.
@@ -86,7 +42,9 @@ function decoration(
   const end = item.end;
   const currentVersion = item.value;
 
-  const hasLatest = (currentVersion) ? checkHasLatest(versions, currentVersion) : false;
+  const hasLatest =
+    versions[0] === currentVersion ||
+    versions[0].indexOf(`${currentVersion}.`) === 0;
 
   const hoverMessage = new MarkdownString(`**Available Versions**`);
   hoverMessage.isTrusted = true;
