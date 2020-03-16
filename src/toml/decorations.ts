398a69ef3912cdfaf6aeee793bcf0afbbf9c3663
Seray Uzgur <serayuzgur@gmail.com>
2020-03-16 17:13:33 +0300

    #72 DOC.RS links

--- src/toml/decorations.ts
+++ src/toml/decorations.ts
@@ -48,7 +48,10 @@ function decoration(
   const currentVersion = item.value;
   const hasLatest = satisfies(versions[0], currentVersion || "0.0.0");
 
-  const hoverMessage = error ? new MarkdownString(`**${error}**`) : new MarkdownString(`**Available Versions**`);
+  const hoverMessage = error ? new MarkdownString(`**${error}**`) : new MarkdownString(`### Docs`);
+  hoverMessage.appendMarkdown(`\n * [Latest](https://docs.rs/crate/${item.key}/${versions[0]})`);
+  hoverMessage.appendMarkdown(`\n * [Current](https://docs.rs/crate/${item.key}/${currentVersion}) \n\n`);
+  hoverMessage.appendMarkdown("### Available Versions");
   hoverMessage.isTrusted = true;
 
   if (versions.length > 0) {
@@ -67,7 +70,7 @@ function decoration(
       end,
     };
     const encoded = encodeURI(JSON.stringify(replaceData));
-    const command = `[${version}](command:crates.replaceVersion?${encoded})`;
+    const command = `[${version}](command:crates.replaceVersion?${encoded}) [   (docs)](https://docs.rs/crate/${item.key}/${version})`;
     hoverMessage.appendMarkdown("\n * ");
     hoverMessage.appendMarkdown(command);
   }
