5edc954bb1fdf0b530f2e59536944e225d709c32
Ervin Oro <ervinoro@gmail.com>
2020-04-12 11:38:21 +0300

    Add support for CARGO_HOME and larger buffer

--- src/api/local_registry.ts
+++ src/api/local_registry.ts
@@ -1,12 +1,24 @@
+// Please note that the internal structure of the Cargo home is not stabilized and may be subject to change at any time.
+//
+// Nevertheless, this api depends on there being a headless crates.io-index git repo at:
+// CARGO_HOME/registry/index/github.com-1ecc6299db9ec823/.git/
+// The repo isn't updated, and is instead assumed to be updated by cargo reasonably recently.
+//
+// Furthermore, this api depends on 'git' command being in PATH
+
 import os = require('os');
 import path = require("path");
 import util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
-const gitDir = path.resolve(os.homedir(), ".cargo/registry/index/github.com-1ecc6299db9ec823/.git/");
+const cargoHome = process.env.CARGO_HOME || path.resolve(os.homedir(), ".cargo/");
+const gitDir = path.resolve(cargoHome, "registry/index/github.com-1ecc6299db9ec823/.git/");
 
 export const versions = (name: string, githubToken?: string) => {
-  return exec(`git --no-pager --git-dir=${gitDir} show origin/master:${decidePath(name)}`)
+  return exec(
+      `git --no-pager --git-dir=${gitDir} show origin/master:${decidePath(name)}`,
+      {maxBuffer: 8 * 1024 * 1024}  // "8M ought to be enough for anyone."
+  )
   .then((buf: {stdout: Buffer, stderr: Buffer}) => {
     const response = buf.stdout.toString();
     const conv = response.split("\n");
