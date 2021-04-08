7bb6904ebc6bec67126ef58fe21c4d45c46d3ce6
William Venner <william@venner.io>
2021-04-08 13:23:15 +0100

    Improve filtering logic and support tabled headers

--- src/providers/autoCompletion.ts
+++ src/providers/autoCompletion.ts
@@ -15,7 +15,7 @@ import { fetchedDepsMap } from "../core/listener";
 import { checkVersion } from "../semver/semverUtils";
 
 const RE_VERSION_AUTO_COMPLETE = /^[ \t]*(?<!#)(\S+?)([ \t]*=[ \t]*)(?:({.*?version[ \t]*=[ \t]*)("|')(.*?)\4|("|')(.*?)\6)/;
-const RE_FEATURES_AUTO_COMPLETE = /^[ \t]*(?<!#)([\S]+?)([ \t]*=[ \t]*.*?{.*?features[ \t]*=[ \t]*\[[ \t]*)(.+?)[ \t]*\]/;
+const RE_FEATURES_AUTO_COMPLETE = /^[ \t]*(?<!#)((?:[\S]+?[ \t]*=[ \t]*.*?{.*?)?features[ \t]*=[ \t]*\[[ \t]*)(.+?)[ \t]*\]/;
 
 const alphabet = "abcdefghijklmnopqrstuvwxyz";
 export function sortText(i: number): string {
@@ -25,6 +25,57 @@ export function sortText(i: number): string {
   return "z".repeat(columns) + letter;
 }
 
+const RE_TABLE_HEADER = /^[ \t]*(?!#)[ \t]*\[[ \t]*(.+?)[ \t]*\][ \t]*$/;
+const RE_TABLE_HEADER_DEPENDENCY = /^(?:.+?\.)?dependencies(?:\.([^.]+?))?$/;
+function findCrate(document: TextDocument, line: number): string | undefined {
+  while (--line >= 0) {
+    const match = document.lineAt(line).text.match(RE_TABLE_HEADER);
+    if (!match) continue;
+    return match[1].match(RE_TABLE_HEADER_DEPENDENCY)?.[1];
+  }
+}
+
+function findCrateAndVersion(document: TextDocument, line: number): [string, string] | undefined {
+  let crate;
+  let version;
+
+  var i = line;
+  while (!crate && --i >= 0) {
+    const lineText = document.lineAt(i).text;
+    const match = lineText.match(RE_TABLE_HEADER);
+    if (!match) {
+      if (!version) {
+        let versionMatch = lineText.match(RE_VERSION_AUTO_COMPLETE);
+        if (versionMatch && versionMatch[1] === "version") {
+          version = versionMatch[7];
+        }
+      }
+    } else {
+      crate = match[1].match(RE_TABLE_HEADER_DEPENDENCY)?.[1];
+    }
+  }
+
+  var i = line;
+  while (!version && ++i < document.lineCount) {
+    const lineText = document.lineAt(i).text;
+    const match = lineText.match(RE_TABLE_HEADER);
+    if (!match) {
+      if (!version) {
+        let versionMatch = lineText.match(RE_VERSION_AUTO_COMPLETE);
+        if (versionMatch && versionMatch[1] === "version") {
+          version = versionMatch[7];
+        }
+      }
+    } else {
+      return;
+    }
+  }
+
+  if (crate && version) {
+    return [crate, version];
+  }
+}
+
 export class VersionCompletions implements CompletionItemProvider {
   provideCompletionItems(
     document: TextDocument,
@@ -32,23 +83,26 @@ export class VersionCompletions implements CompletionItemProvider {
     _token: CancellationToken,
     _context: CompletionContext
   ): ProviderResult<CompletionItem[] | CompletionList> {
-    if (!fetchedDepsMap) return [];
+    if (!fetchedDepsMap) return;
 
-    const line = document.lineAt(position);
-
-    const match = line.text.match(RE_VERSION_AUTO_COMPLETE);
+    const match = document
+      .lineAt(position)
+      .text.match(RE_VERSION_AUTO_COMPLETE);
     if (match) {
-      const crate = match[1];
+      const tabledHeaderCrateName = findCrate(document, position.line);
+      if (tabledHeaderCrateName && match[1] !== "version") return;
+      const crate = tabledHeaderCrateName ?? match[1];
+
       const version = match[7] ?? match[5];
 
       const fetchedDep = fetchedDepsMap.get(crate);
       if (!fetchedDep || !fetchedDep.versions) return;
 
-      const versionStart = crate.length + match[2].length + (match[3]?.length ?? 0) + 1;
+      const versionStart = match[1].length + match[2].length + (match[3]?.length ?? 0) + 1;
       const versionEnd = versionStart + version.length;
 
       if (version.trim().length !== 0) {
-        const filterVersion = version.toLowerCase();
+        const filterVersion = version.substr(0, versionStart - position.character).toLowerCase();
 
         const range = new Range(
           new Position(position.line, versionStart),
@@ -57,24 +111,22 @@ export class VersionCompletions implements CompletionItemProvider {
 
         let i = 0;
         return new CompletionList(
-          fetchedDep.versions
-            .filter((version) =>
-              version.toLowerCase().startsWith(filterVersion)
-            )
-            .map((version) => {
-              const item = new CompletionItem(
-                version,
-                CompletionItemKind.Class
-              );
-              item.range = range;
-              item.preselect = i === 0;
-              item.sortText = sortText(i++);
-              return item;
-            }),
+          (filterVersion.length > 0
+            ? fetchedDep.versions.filter((version) =>
+                version.toLowerCase().startsWith(filterVersion)
+              )
+            : fetchedDep.versions
+          ).map((version) => {
+            const item = new CompletionItem(version, CompletionItemKind.Class);
+            item.range = range;
+            item.preselect = i === 0;
+            item.sortText = sortText(i++);
+            return item;
+          }),
           true
         );
       } else if (position.character !== versionEnd + 1) {
-        // Fixes the edge case where auto completion comes up for version = ""|
+        // Fixes the edge case where auto completion comes up for `version = ""|`
         return fetchedDep.versionCompletionItems;
       }
     }
@@ -88,23 +140,32 @@ export class FeaturesCompletions implements CompletionItemProvider {
     _token: CancellationToken,
     _context: CompletionContext
   ): ProviderResult<CompletionItem[] | CompletionList> {
-    if (!fetchedDepsMap) return [];
+    if (!fetchedDepsMap) return;
 
     const line = document.lineAt(position);
 
     const featuresMatch = line.text.match(RE_FEATURES_AUTO_COMPLETE);
-    const versionMatch = line.text.match(RE_VERSION_AUTO_COMPLETE);
-    if (featuresMatch && versionMatch) {
-      const crate = featuresMatch[1];
-      const version = versionMatch[7] ?? versionMatch[5];
+    if (featuresMatch) {
+      const match = findCrateAndVersion(document, position.line);
+      if (!match) return;
+      const [crate, version] = match;
 
       const fetchedDep = fetchedDepsMap.get(crate);
-      if (!fetchedDep || !fetchedDep.featureCompletionItems || !fetchedDep.versions) return;
+      if (
+        !fetchedDep ||
+        !fetchedDep.featureCompletionItems ||
+        !fetchedDep.versions
+      )
+        return;
 
-      const featuresArray = featuresMatch[3];
+      const featuresArray = featuresMatch[2];
+
+      const featuresStart = featuresMatch[1].length;
+      const featuresEnd = featuresStart + featuresArray.length;
+
       const featuresRange = new Range(
-        new Position(position.line, crate.length + featuresMatch[2].length),
-        new Position(position.line, crate.length + featuresMatch[2].length + featuresArray.length)
+        new Position(position.line, featuresStart),
+        new Position(position.line, featuresEnd)
       );
 
       if (!featuresRange.contains(position)) return;
